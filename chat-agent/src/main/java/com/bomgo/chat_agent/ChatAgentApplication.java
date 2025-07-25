25893486bd350394ee1ea980b58a539f205c859c
i.matheuslmaia <i.matheuslmaia@gmail.com>
2025-07-25 18:17:46 +0000

    validando endpoint do webhook da meta

--- chat-agent/src/main/java/com/bomgo/chat_agent/ChatAgentApplication.java
+++ chat-agent/src/main/java/com/bomgo/chat_agent/ChatAgentApplication.java
@@ -1,11 +1,17 @@
 package com.bomgo.chat_agent;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.web.util.HeaderUtils;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.messaging.Message;
+import org.springframework.web.server.ResponseStatusException;
 
 @SpringBootApplication
 public class ChatAgentApplication {
@@ -17,9 +23,34 @@ public class ChatAgentApplication {
 		SpringApplication.run(ChatAgentApplication.class, args);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Bean
-	public Function<String, String> webhookValidation() {
-		System.out.println("oi");
-		return input -> input.toUpperCase();
+	public Function<Message<Object>, String> webhookValidation() {
+		return input -> {
+			Map<String, String> params = new HashMap<>();
+
+			try {
+				params = (Map<String, String>) input.getHeaders()
+					.getOrDefault(HeaderUtils.HTTP_REQUEST_PARAM, new HashMap<String, String>());
+
+				if (input.getPayload().equals("")) {
+					String challenge = params.get("hub.challenge");
+					String verifyToken = params.get("hub.verify_token");
+					String mode = params.get("hub.mode");
+
+	
+					if (verifyToken.equals(this.metaWebhookValidateToken) && mode.equals("subscribe")) {
+						return challenge;
+
+					} else {
+						throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "invalid verify token");
+					}
+				}
+				return "";
+	
+			} catch (Exception e) {
+				throw new RuntimeException("error validating webhook");
+			}
+		};
 	}
 }
